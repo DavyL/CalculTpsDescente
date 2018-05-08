245bd959a44ec4f37a4b95f5ef1463597a5c5f09
DavyL <cr4zypxl@gmail.com>
2018-05-08 22:25:57 +0200

    Ajouts de plusieurs fonctionnalités (ex : script latex) + ajout du début de la partie app num sur le tex/pdf

--- main.py
+++ main.py
@@ -24,33 +24,34 @@ else:
 # latex  : pour avoir en sortie du programme un fichier .tex contenant un résumé de l'étude des fonctions
 # size [int]: pour définir la taille de la subdivision lors de l'execution
 
+#IMPORTANT : l'option latex ignore l'option size
+
 #On note:
 #nombreDeSubdivision : le nombre de subidivision, noté N dans l'énoncé
 #fonction : la fonction du toboggan
 #pas : la taille d'un pas, noté delta_x dans l'énoncé
 #listeDePoints : liste des x_i avec la notation de l'énoncé
+#tempsDescenteListe : tableau contenant pour chaque fonction, le calcul du temps de descente en fonction du nombre de points de la subdivision
+latex = 0
+nombreDePointListe = []
+tempsDescenteListe = []
+tempsDescente = []
+if( "latex" in args):
+    latex = 1
+    nombreDePointListe = [101, 501, 2001, 10001, 300001]
 
-if( "size" not in args):
-    nombreDePoint=int(input("nombre de points ")) # il y a un point de plus que le nombre de subdivision
 else:
-    flag = 0
-    for arg in args: 
-        if flag:
-            nombreDePoint = int(arg)
-            flag = 0
-        if arg == "size":
-            flag = 1
+    if( "size" not in args):
+        nombreDePointListe.append(int(input("nombre de points "))) # il y a un point de plus que le nombre de subdivision
+    else:
+        flag = 0
+        for arg in args: 
+            if flag:
+                nombreDePointListe.append(int(arg))
+                flag = 0
+            if arg == "size":
+                flag = 1
 
-                                                                                     
-pas = 1 / (nombreDePoint - 1) 
-listeDePoint =[]
-    
-#Les fonction ne sont pas nécéssairement définies en 0, on utilise donc une méthode ouverte
-#On étudie donc les fonction sur l'ouvert (0, 1) sur lequel on suppose les fonction définies
-
-for i in range (0, nombreDePoint ):
-    xi = i*pas + 1/nombreDePoint
-    listeDePoint.append(xi) 
 #On crée une liste de fonctions, de derivées et de fonctions correspondant aux integrales resp, noté integrale
 
 fonction = []
@@ -112,61 +113,95 @@ fonction.append( lambda x: 1 - ((x**(3/2))*(5-3*x))/2)
 derive.append( lambda x: -1)
 derive.append( lambda x: -1/(2*sqrt(x)))
 derive.append( lambda x: (15/4)*(-1 * x**(1/2) + x**(3/2)))
-
-#integrale est la fonction à intégrer
-#Dans le calcul de la dérivée, on divise le pas par deux afin de rendre le calcul de la dérivée plus précis que celui de l'integrale
-g = 10
-
 #Dans la boucle suivante, on parcourt chaque tableau de fonction défini précedemment
-for k in range(len(fonction)):
+for k in range(len(fonction)): 
+    tempsDescenteListe = []
     print("\n Etude de la fonction " + str(k))
     integrale.append( lambda x: sqrt(1 + (derive[k](x))**2)/(sqrt(2*g*(fonction[k](0) - fonction[k](x)))))
 
-    #integrale = lambda x: sqrt(1 + (deriv(fonction, x, 0.5 * pas))**2)/(sqrt(2*(g*fonction(0) - fonction(x))))
 
-    #méthode rect à gauche
-    somme = 0
-    listeRG = [0]
+    #integrale est la fonction à intégrer
+    #Dans le calcul de la dérivée, on divise le pas par deux afin de rendre le calcul de la dérivée plus précis que celui de l'integrale
+    g = 10
 
-    for i in range (0, nombreDePoint-1):
-        #on décale de 1 les valeurs dans la somme car i est l'indice dans la liste
-        somme = somme + integrale[k] (listeDePoint[i])
-        tempsDeDescente = pas * somme
+    for nombreDePoint in nombreDePointListe:   
+        print(nombreDePoint, nombreDePointListe)
+        pas = 1 / (nombreDePoint - 1) 
+        listeDePoint =[]
+        tempsDescente = []
+            
+        #Les fonction ne sont pas nécéssairement définies en 0, on utilise donc une méthode ouverte
+        #On étudie donc les fonction sur "l'ouvert" (0, 1) sur lequel on suppose les fonction définies
 
-    print ("tempsDeDescente avec la méthode des rectangles à gauche=", tempsDeDescente)
+        for i in range (0, nombreDePoint ):
+            xi = i*pas + 1/nombreDePoint
+            listeDePoint.append(xi) 
+        
+        #méthode rect à gauche
+        somme = 0
+        listeRG = [0]
 
-    #méthode rect à droite
-    somme = 0 
-    listeRD = [0]
+        for i in range (0, nombreDePoint-1):
+            #on décale de 1 les valeurs dans la somme car i est l'indice dans la liste
+            somme = somme + integrale[k] (listeDePoint[i])
+        tempsDescente.append(pas * somme)
+        print ("tempsDeDescente avec la méthode des rectangles à gauche=", tempsDescente[-1])
 
-    for i in range (1, nombreDePoint):
-        somme = somme + integrale[k] (listeDePoint[i]) 
-        tempsDeDescente = pas * somme 
+        #méthode rect à droite
+        somme = 0 
+        listeRD = [0]
 
-    print ("tempsDeDescente avec la méthode des rectangles à droite=", tempsDeDescente)
+        for i in range (1, nombreDePoint):
+            somme = somme + integrale[k] (listeDePoint[i]) 
+        tempsDescente.append(pas * somme) 
 
-    #méthode des trapèzes
-    somme = 0 
+        print ("tempsDeDescente avec la méthode des rectangles à droite=", tempsDescente[-1])
 
-    for i in range ( 1, nombreDePoint - 1):
-        somme = somme + integrale[k] (listeDePoint[i])
-    tempsDeDescente = pas*((integrale[k] (listeDePoint[0]) + integrale[k] (listeDePoint[nombreDePoint - 1])) / 2 + somme)
+        #méthode des trapèzes
+        somme = 0 
 
-    print ("tempsDeDescente avec la méthode des trapèzes =", tempsDeDescente)
-    #méthode de Simpson
-    if nombreDePoint%2 != 0:
-        somme1 = 0
-        somme2 = 0
-        m = nombreDePoint // 2
+        for i in range ( 1, nombreDePoint - 1):
+            somme = somme + integrale[k] (listeDePoint[i])
+        tempsDescente.append(pas*((integrale[k] (listeDePoint[0]) + integrale[k] (listeDePoint[nombreDePoint - 1])) / 2 + somme))
 
-        for i in range(1, m):
-            somme1= somme1 + integrale[k](listeDePoint[2*i])
-            somme2= somme2 + integrale[k](listeDePoint[2*i+1])
-        tempsDeDescente = (pas / 3) * (integrale[k](listeDePoint[0])+ 2 * somme1 + 4 * somme2 + integrale[k](listeDePoint[nombreDePoint-1]) )
-        print ("tempsDeDescente avec la méthode de Simpson = ", tempsDeDescente)
+        print ("tempsDeDescente avec la méthode des trapèzes =", tempsDescente[-1])
+        #méthode de Simpson
+        if nombreDePoint%2 != 0:
+            somme1 = 0
+            somme2 = 0
+            m = nombreDePoint // 2
 
-    else:
-        print("Le nombre de points de la subidivision n'est pas adapté à la méthode de Simpson")
+            for i in range(1, m):
+                somme1= somme1 + integrale[k](listeDePoint[2*i])
+                somme2= somme2 + integrale[k](listeDePoint[2*i+1])
+            tempsDescente.append((pas / 3) * (integrale[k](listeDePoint[0])+ 2 * somme1 + 4 * somme2 + integrale[k](listeDePoint[nombreDePoint-1]) ))
+            print ("tempsDeDescente avec la méthode de Simpson = ", tempsDescente[-1])
+
+        else:
+            print("Le nombre de points de la subidivision n'est pas adapté à la méthode de Simpson")
+        
+        tempsDescenteListe.append(tempsDescente)
+        
+    ################
+    #ECRITURE LATEX#
+    ################
+    if(latex):
+        fichierLatex = open("tableau"+str(k)+".tex","w")                  #On crée un fichier "tableau.tex" qui contient du code LaTeX (en mode écriture)
+              
+        fichierLatex.write("\\begin{center}\n")                  #
+        fichierLatex.write("\t \\begin{tabular}{fonction " + str(k) +" |Rect. \`a gauche | Rect. \`a droite | Trap\`eze | Simpson |} \n")               #
+        fichierLatex.write("\t\t \\hline\n")   #
+        for l in range(len(nombreDePointListe)):
+            fichierLatex.write( "\t\t\tN = " + str(nombreDePointListe[l]) 
+                                                            + " & " + str(tempsDescenteListe[l][0])
+                                                            + " & " + str(tempsDescenteListe[l][1])
+                                                            + " & " + str(tempsDescenteListe[l][2])
+                                                            + " & " + str(tempsDescenteListe[l][3])
+                                                            + " \\\\ \\hline \n")
+        fichierLatex.write("\t \\end{tabular}\n")
+        fichierLatex.write("\\end{center}\n")
+        fichierLatex.close() 
+
     ####################
     #AFFICHAGE FONCTION#
     ####################
@@ -175,8 +210,7 @@ for k in range(len(fonction)):
     plt.figure(1)                                                                  
     plt.plot(listeDePoint, [fonction[k](i*pas) for i in range(nombreDePoint)], "ro")
     plt.ylabel("Fonction[" + str(k) + "]")
-
+    
+        
 #On affiche les figures crées ci-dessus
 plt.show()
-
-
